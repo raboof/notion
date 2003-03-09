a010ee3cc4a1963d0384963d7c79a5600c46c61d
tuomov <arnouten@bzzt.net>
2003-03-09 13:41:59 +0100

    trunk: changeset 343
    *sigh*
    
    darcs-hash:20030309124159-e481e-511a25437258ad58868c1a1af904d1cfc4e8df49.gz

---
+++ floatws/funtabs.h
@@ -0,0 +1,19 @@
+/*
+ * ion/floatws/funtabs.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2003. 
+ * See the included file LICENSE for details.
+ */
+
+#ifndef ION_FLOATWS_FUNTABS_H
+#define ION_FLOATWS_FUNTABS_H
+
+#include <ioncore/function.h>
+#include <ioncore/binding.h>
+
+extern WBindmap floatws_bindmap;
+extern WBindmap floatframe_bindmap;
+extern WFunclist floatws_funclist;
+extern WFunclist floatframe_funclist;
+
+#endif /* ION_FLOATWS_FUNTABS_H */
