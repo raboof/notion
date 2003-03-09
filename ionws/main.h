e5cf61cb09a299542c0d3860c745000c95abbf93
tuomov <arnouten@bzzt.net>
2003-03-09 13:24:52 +0100

    trunk: changeset 341
    Added IonWs main.h
    
    darcs-hash:20030309122452-e481e-d52f094670f0586d801f65bdf60f3d9fab429daa.gz

---
+++ ionws/main.h
@@ -0,0 +1,14 @@
+/*
+ * ion/ionws/main.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2003. 
+ * See the included file LICENSE for details.
+ */
+
+#ifndef ION_IONWS_MAIN_H
+#define ION_IONWS_MAIN_H
+
+extern bool ionws_module_init();
+extern void ionws_module_deinit();
+
+#endif /* ION_IONWS_MAIN_H */
