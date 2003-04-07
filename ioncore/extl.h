dfb6d7d50fcfa032a8d3b3e23902f33d1c497af9
tuomov <arnouten@bzzt.net>
2003-04-07 07:23:17 +0200

    trunk: changeset 384
    Added extl.h
    
    darcs-hash:20030407052317-e481e-8d6efaf130889a966847fc7f9d4002272f129407.gz

---
+++ ioncore/extl.h
@@ -0,0 +1,13 @@
+/*
+ * ion/ioncore/extl.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2003. 
+ * See the included file LICENSE for details.
+ */
+
+#ifndef ION_IONCORE_EXTL_H
+#define ION_IONCORE_EXTL_H
+
+#include "../luaextl/luaextl.h"
+
+#endif /* ION_IONCORE_EXTL_H */
