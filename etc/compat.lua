491fefa5d94ab328d78037470aad4514359547fb
tuomov <arnouten@bzzt.net>
2003-04-22 21:08:34 +0200

    trunk: changeset 458
    Some comments added
    
    darcs-hash:20030422190834-e481e-f81a42537c847c33037de075b2c925e9ae10de90.gz

--- etc/compat.lua
+++ etc/compat.lua
@@ -1,8 +1,11 @@
 --
 -- Compatibility functions to make transition from older (yet Lua-enabled)
--- versions of Ion easier.
+-- versions of Ion easier. However, these definitions will gradually be
+-- removed (say, about a month after addition) to force eventual conversion
+-- of configuration files.
 -- 
 
+-- Added 2003-04-22
 floatframe_raise=region_raise
 floatframe_lower=region_lower
 
