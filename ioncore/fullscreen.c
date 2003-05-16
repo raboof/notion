71976bed60c87819ef65231e5b702b40e4bc7a76
tuomov <arnouten@bzzt.net>
2003-05-16 17:42:33 +0200

    trunk: changeset 593
    Moved fullscreen stuff into a separate file.
    
    darcs-hash:20030516154233-e481e-e5a70bb85b0cc3ca4052cc3fa58393fcc338c935.gz

---
+++ ioncore/fullscreen.c
@@ -0,0 +1,165 @@
+/*
+ * ion/ioncore/fullscreen.c
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2003. 
+ * 
+ * See the included file LICENSE for details.
+ */
+
+#include "common.h"
+#include "global.h"
+#include "sizehint.h"
+#include "clientwin.h"
+#include "attach.h"
+#include "viewport.h"
+#include "manage.h"
+#include "fullscreen.h"
+#include "mwmhints.h"
+
+
+bool clientwin_check_fullscreen_request(WClientWin *cwin, int w, int h)
+{
+	WRegion *reg;
+	WMwmHints *mwm;
+	
+	mwm=get_mwm_hints(cwin->win);
+	if(mwm==NULL || !(mwm->flags&MWM_HINTS_DECORATIONS) ||
+	   mwm->decorations!=0)
+		return FALSE;
+	
+	FOR_ALL_MANAGED_ON_LIST(SCREEN_OF(cwin)->viewport_list, reg){
+		if(!WOBJ_IS(reg, WViewport))
+			continue;
+		/* TODO: if there are multiple possible screens, use the one with
+		 * requested position, if any.
+		 */
+		if(REGION_GEOM(reg).w==w && REGION_GEOM(reg).h==h){
+			return clientwin_fullscreen_vp(cwin, (WViewport*)reg,
+										   clientwin_get_switchto(cwin));
+		}
+	}
+	
+	/* Catch Xinerama-unaware apps here */
+	if(REGION_GEOM(SCREEN_OF(cwin)).w==w && REGION_GEOM(SCREEN_OF(cwin)).h==h){
+		return clientwin_enter_fullscreen(cwin, 
+										  clientwin_get_switchto(cwin));
+	}
+	return FALSE;
+}
+
+
+static void lastmgr_watchhandler(WWatch *watch, WObj *obj)
+{
+	WClientWinFSInfo *fsinfo=(WClientWinFSInfo*)watch;
+	WRegion *r;
+	
+	assert(WOBJ_IS(obj, WRegion));
+	
+	r=region_find_rescue_manager((WRegion*)obj);
+	
+	if(r!=NULL)
+		setup_watch(&(fsinfo->last_mgr_watch), (WObj*)r, lastmgr_watchhandler);
+}
+
+
+bool clientwin_fullscreen_vp(WClientWin *cwin, WViewport *vp, bool switchto)
+{
+	int rootx, rooty;
+	bool wasfs=TRUE;
+	
+	if(cwin->fsinfo.last_mgr_watch.obj==NULL){
+		wasfs=FALSE;
+		
+		if(REGION_MANAGER(cwin)!=NULL){
+			setup_watch(&(cwin->fsinfo.last_mgr_watch),
+						(WObj*)REGION_MANAGER(cwin),
+						lastmgr_watchhandler);
+		}else if(vp->current_ws!=NULL){
+			setup_watch(&(cwin->fsinfo.last_mgr_watch),
+						(WObj*)vp->current_ws, 
+						lastmgr_watchhandler);
+		}
+		region_rootpos((WRegion*)cwin, &rootx, &rooty);
+		cwin->fsinfo.saved_rootrel_geom.x=rootx;
+		cwin->fsinfo.saved_rootrel_geom.y=rooty;
+		cwin->fsinfo.saved_rootrel_geom.w=REGION_GEOM(cwin).w;
+		cwin->fsinfo.saved_rootrel_geom.h=REGION_GEOM(cwin).h;
+	}
+	
+	if(!region_add_managed_simple((WRegion*)vp, (WRegion*)cwin, switchto ? 
+								  REGION_ATTACH_SWITCHTO : 0)){
+		warn("Failed to enter full screen mode");
+		if(!wasfs)
+			reset_watch(&(cwin->fsinfo.last_mgr_watch));
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+bool clientwin_enter_fullscreen(WClientWin *cwin, bool switchto)
+{
+	WViewport *vp=region_viewport_of((WRegion*)cwin);
+	
+	if(vp==NULL){
+		vp=SCREEN_OF(cwin)->current_viewport;
+		if(vp==NULL)
+			return FALSE;
+	}
+	
+	return clientwin_fullscreen_vp(cwin, vp, switchto);
+}
+
+
+bool clientwin_leave_fullscreen(WClientWin *cwin, bool switchto)
+{	
+	WRegion *reg;
+	WAttachParams param;
+	XSizeHints hnt;
+	int rootx, rooty;
+	
+	if(cwin->fsinfo.last_mgr_watch.obj==NULL)
+		return FALSE;
+
+	reg=(WRegion*)cwin->fsinfo.last_mgr_watch.obj;
+	reset_watch(&(cwin->fsinfo.last_mgr_watch));
+	assert(WOBJ_IS(reg, WRegion));
+	
+	/* Set up geometry hints */
+	param.flags|=(REGION_ATTACH_SIZERQ|REGION_ATTACH_POSRQ|
+				  REGION_ATTACH_SWITCHTO|REGION_ATTACH_SIZE_HINTS);
+	
+	region_rootpos(reg, &rootx, &rooty);
+	param.geomrq=cwin->fsinfo.saved_rootrel_geom;
+	param.geomrq.x-=rootx;
+	param.geomrq.y-=rooty;
+	
+	hnt=cwin->size_hints;
+	hnt.flags|=PWinGravity;
+	hnt.win_gravity=StaticGravity;
+	param.size_hints=&hnt;
+	
+	if(!do_add_clientwin(reg, cwin, &param)){
+		warn("WClientWin failed to return from full screen mode; remaining "
+			 "manager or parent from previous location refused to manage us.");
+		return FALSE;
+	}
+	
+	return region_goto((WRegion*)cwin);
+}
+
+
+/*EXTL_DOC
+ * Toggle between full screen and normal (framed) mode.
+ */
+EXTL_EXPORT
+bool clientwin_toggle_fullscreen(WClientWin *cwin)
+{
+	if(cwin->fsinfo.last_mgr_watch.obj!=NULL)
+		return clientwin_leave_fullscreen(cwin, TRUE);
+	
+	return clientwin_enter_fullscreen(cwin, TRUE);
+}
+
+
---
+++ ioncore/fullscreen.h
@@ -0,0 +1,22 @@
+/*
+ * ion/ioncore/fullscreen.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2003. 
+ * See the included file LICENSE for details.
+ */
+
+#ifndef ION_IONCORE_FULLSCREEN_H
+#define ION_IONCORE_FULLSCREEN_H
+
+#include "common.h"
+#include "viewport.h"
+#include "clientwin.h"
+
+extern bool clientwin_check_fullscreen_request(WClientWin *cwin, int w, int h);
+extern bool clientwin_fullscreen_vp(WClientWin *cwin, WViewport *vp,
+									bool switchto);
+extern bool clientwin_toggle_fullscreen(WClientWin *cwin);
+extern bool clientwin_enter_fullscreen(WClientWin *cwin, bool switchto);
+extern bool clientwin_leave_fullscreen(WClientWin *cwin, bool switchto);
+
+#endif /* ION_IONCORE_FULLSCREEN_H */
