b11acfa28fcf17ef7bd2c1eef214384c0b3a65c3
tuomov <arnouten@bzzt.net>
2003-05-31 15:20:09 +0200

    trunk: changeset 690
    Oops.
    
    darcs-hash:20030531132009-e481e-f73caa913838e3cd985bb82c66fed7baed41cd6f.gz

--- system.mk
+++ system.mk
@@ -7,7 +7,7 @@
 ## Installation paths
 ##
 
-PREFIX=/usr/local/stow/ion-20030524
+PREFIX=/usr/local/ion-devel
 
 # Unless you are creating a package conforming to some OS's standards, you
 # probably do not want to modify the following directories:
