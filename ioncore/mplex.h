13f1a81570fe1bd5363ff3e7852c87cf0e98c9b8
tuomov <arnouten@bzzt.net>
2003-06-09 18:14:45 +0200

    trunk: changeset 725
    Added some definitions in header
    
    darcs-hash:20030609161445-e481e-593e05fb9f18208e41fa5f597cf4982309042c3c.gz

--- ioncore/mplex.h
+++ ioncore/mplex.h
@@ -11,6 +11,7 @@
 #include "common.h"
 #include "window.h"
 #include "attach.h"
+#include "extl.h"
 
 #define WMPLEX_ADD_TO_END 0x0001
 #define WMPLEX_MANAGED_UNVIEWABLE 0x0002
@@ -46,6 +47,7 @@ extern WRegion *mplex_add_input(WMPlex *mplex, WRegionAddFn *fn,
 								void *fnp);
 extern void mplex_remove_managed(WMPlex *mplex, WRegion *reg);
 extern void mplex_fit_managed(WMPlex *mplex);
+extern void mplex_attach_tagged(WMPlex *mplex);
 
 /* Switch */
 extern bool mplex_display_managed(WMPlex *mplex, WRegion *sub);
@@ -58,6 +60,8 @@ extern void mplex_focus(WMPlex *mplex, bool warp);
 
 /* Misc */
 extern WRegion *mplex_nth_managed(WMPlex *mplex, uint n);
+extern int mplex_managed_count(WMPlex *mplex);
+extern ExtlTab mplex_managed_list(WMPlex *mplex);
 extern WRegion *mplex_current(WMPlex *mplex);
 extern WRegion *mplex_current_input(WMPlex *mplex);
 extern void mplex_move_current_right(WMPlex *mplex);
@@ -69,6 +73,5 @@ DYNFUN void mplex_size_changed(WMPlex *mplex, bool wchg, bool hchg);
 DYNFUN void mplex_managed_changed(WMPlex *mplex, bool sw);
 DYNFUN void mplex_managed_added(WMPlex *mplex, WRegion *nreg, bool sw);
 DYNFUN void mplex_managed_removed(WMPlex *mplex, WRegion *oreg, bool sw);
-
 
 #endif /* ION_IONCORE_MPLEX_H */
