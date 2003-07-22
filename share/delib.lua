7998f82c5e8f7dc8cc68aa1ce139bb917a2b9508
tuomov <arnouten@bzzt.net>
2003-07-22 20:09:31 +0200

    trunk: changeset 822
    Drawing engine Lua library was added.
    
    darcs-hash:20030722180931-e481e-e9f219f6b7dcc2b103aaee979e58ebac094cbee2.gz

---
+++ share/delib.lua
@@ -0,0 +1,66 @@
+--
+-- ion/share/delib.lua -- Helper functions for defining drawing engine 
+-- styles
+-- 
+-- Copyright (c) Tuomo Valkonen 2003.
+--
+-- Ion is free software; you can redistribute it and/or modify it under
+-- the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation; either version 2.1 of the License, or
+-- (at your option) any later version.
+--
+
+local stylecache={}
+
+local function base_on(name, list)
+    if not stylecache[list.based_on] then
+        warn("Attempt to base style "..name.." on style "..list.based_on
+             .." that is not defined.")
+        return false
+    end
+
+    for k, v in stylecache[list.based_on] do
+        if type(k)=="number" then
+            table.insert(list, v)
+        elseif not list[k] then
+            list[k]=v
+        end
+    end
+    
+    return true
+end
+
+
+function de_define_style(name, list)    
+    if not list then
+        return function(list2)
+                   de_define_style(name, list2)
+               end
+    end
+    
+    if list.based_on then
+        if not base_on(name, list) then
+            return
+        end
+    end
+    
+    stylecache[name]=list
+    
+    for xscr, rootwin in root_windows() do
+        if not de_do_define_style(rootwin, name, list) then
+            break
+        end
+    end
+end
+
+
+function de_substyle(pattern, list)
+    if not list then
+        return function(list2)
+                   substyle(pattern, list2)
+               end
+    end    
+    
+    list.substyle_pattern=pattern
+    return list
+end
