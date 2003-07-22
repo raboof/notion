11cd73a22948c2868b5d321d964de12f1fe62470
tuomov <arnouten@bzzt.net>
2003-07-22 20:06:06 +0200

    trunk: changeset 821
    Drawing engine interface code was added although not yet used.
    
    darcs-hash:20030722180606-e481e-ef22535ef5afa2e40b9799fe5567dbefe1f8a53b.gz

---
+++ ioncore/gr.c
@@ -0,0 +1,452 @@
+/*
+ * ion/ioncore/gr.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2003. 
+ *
+ * Ion is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <string.h>
+
+#include "common.h"
+#include "global.h"
+#include "hooks.h"
+#include "modules.h"
+#include "readconfig.h"
+#include "gr.h"
+
+
+/*{{{ Lookup and registration */
+
+INTRSTRUCT(GrEngine);
+
+DECLSTRUCT(GrEngine){
+	char *name;
+	GrGetBrushFn *fn;
+	GrGetValuesFn *vfn;
+	GrEngine *next, *prev;
+};
+
+
+static GrEngine *engines=NULL, *current_engine=NULL;
+
+
+bool gr_register_engine(const char *engine, GrGetBrushFn *fn,
+						GrGetValuesFn *vfn)
+{
+	GrEngine *eng;
+	
+	fprintf(stderr, "registering engine %s\n", engine);
+	
+	if(engine==NULL || fn==NULL || vfn==NULL)
+		return FALSE;
+	
+	eng=ALLOC(GrEngine);
+	
+	if(eng==NULL){
+		warn_err();
+		return FALSE;
+	}
+	
+	eng->name=scopy(engine);
+	
+	if(eng->name==NULL){
+		warn_err();
+		free(eng);
+		return FALSE;
+	}
+	
+	eng->fn=fn;
+	eng->vfn=vfn;
+	
+	LINK_ITEM(engines, eng, next, prev);
+
+	fprintf(stderr, "ok\n");
+	
+	return TRUE;
+}
+
+
+void gr_unregister_engine(const char *engine)
+{
+	GrEngine *eng;
+	
+	for(eng=engines; eng!=NULL; eng=eng->next){
+		if(strcmp(eng->name, engine)==0)
+			break;
+	}
+	
+	if(eng==NULL)
+		return;
+	
+	UNLINK_ITEM(engines, eng, next, prev);
+	free(eng->name);
+	if(current_engine==eng)
+		current_engine=NULL;
+	free(eng);
+}
+
+
+static bool gr_do_select_engine(const char *engine)
+{
+	GrEngine *eng;
+
+	for(eng=engines; eng!=NULL; eng=eng->next){
+		if(strcmp(eng->name, engine)==0){
+			current_engine=eng;
+			return TRUE;
+		}
+	}
+	
+	return FALSE;
+}
+
+
+/*EXTL_DOC
+ * Future requests for ''brushes'' are to be forwarded to the drawing engine
+ * \var{engine}. If no engine of such name is known, a module with that name
+ * is attempted to be loaded. This function is only intended to be called from
+ * colour scheme etc. configuration files and can not be used to change the
+ * look of existing objects; for that use \fnref{reread_draw_config}.
+ */
+EXTL_EXPORT
+bool gr_select_engine(const char *engine)
+{
+	if(engine==NULL)
+		return FALSE;
+	
+	fprintf(stderr, "selecting engine %s\n", engine);
+
+	if(gr_do_select_engine(engine))
+		return TRUE;
+	
+	fprintf(stderr, "attempting to load module\n");
+	
+	if(!load_module(engine))
+		return FALSE;
+
+	fprintf(stderr, "success\n");
+	
+	{bool ret=gr_do_select_engine(engine); fprintf(stderr, "-->%d\n", ret);
+		return ret;
+	}
+}
+
+
+GrBrush *gr_get_brush(WRootWin *rootwin, Window win, const char *style)
+{
+	GrEngine *eng=(current_engine!=NULL ? current_engine : engines);
+	GrBrush *ret;
+	
+	fprintf(stderr, "looking up brush for %s\n", style);
+	
+	if(eng==NULL || eng->fn==NULL){
+		fprintf(stderr, "no engine\n");
+		return NULL;
+	}
+	
+	ret=(eng->fn)(rootwin, win, style);
+	
+	fprintf(stderr, "got: %p\n", ret);
+	
+	return ret;
+}
+
+
+bool gr_get_brush_values(WRootWin *rootwin, const char *style,
+						 GrBorderWidths *bdw, GrFontExtents *fnte,
+						 ExtlTab *tab)
+{
+	GrEngine *eng=(current_engine!=NULL ? current_engine : engines);
+	
+	if(eng==NULL || eng->fn==NULL)
+		return FALSE;
+	
+	return (eng->vfn)(rootwin, style, bdw, fnte, tab);
+}
+
+
+/*}}}*/
+
+
+/*{{{ Scoring */
+
+
+uint gr_stylespec_score2(const char *spec, const char *attrib, 
+						 const char *attrib_p2)
+{
+	uint score=0;
+	uint a=0;
+	uint mult=1;
+
+	if(attrib==NULL){
+		if(spec==NULL || strcmp(spec, "*")==0)
+			return 1;
+		return 0;
+	}
+	
+	while(1){
+		if(*spec=='*'){
+			score=score+mult;
+			spec++;
+			attrib=strchr(attrib, '-');
+		}else{
+			while(1){
+				if(*attrib=='\0'){
+					attrib=NULL;
+					break;
+				}
+				if(*attrib=='-')
+					break;
+				if(*spec!=*attrib)
+					return 0;
+				attrib++;
+				spec++;
+			}
+			score=score+2*mult;
+		}
+		
+		if(*spec=='\0')
+			return score;
+		else if(*spec!='-')
+			return 0;
+		
+		if(attrib==NULL){
+			if(a==0 && attrib_p2!=NULL){
+				attrib=attrib_p2;
+				a++;
+			}else{
+				return 0;
+			}
+		}else{
+			attrib++;
+		}
+
+		spec++;
+		mult=mult*3;
+	}
+}
+
+
+uint gr_stylespec_score(const char *spec, const char *attrib)
+{
+	return gr_stylespec_score2(spec, attrib, NULL);
+}
+
+
+/*}}}*/
+
+
+/*{{{ Init, deinit */
+
+
+bool grbrush_init(GrBrush *brush)
+{
+	return TRUE;
+}
+
+
+void grbrush_deinit(GrBrush *brush)
+{
+}
+
+
+void grbrush_release(GrBrush *brush, Window win)
+{
+	CALL_DYN(grbrush_release, brush, (brush, win));
+}
+
+
+GrBrush *grbrush_get_slave(GrBrush *brush, WRootWin *rootwin, 
+						   Window win, const char *style)
+{
+	fprintf(stderr, "Looking up slave %s\n",style);
+	{
+	GrBrush *slave=NULL;
+	CALL_DYN_RET(slave, GrBrush*, grbrush_get_slave, brush,
+				 (brush, rootwin, win, style));
+		fprintf(stderr, "got: %p\n", slave);
+	return slave;
+	}
+}
+
+
+/*}}}*/
+
+
+/*{{{ Dynfuns/values */
+
+
+void grbrush_get_font_extents(GrBrush *brush, GrFontExtents *fnte)
+{
+	CALL_DYN(grbrush_get_font_extents, brush, (brush, fnte));
+}
+
+
+void grbrush_get_border_widths(GrBrush *brush, GrBorderWidths *bdw)
+{
+	CALL_DYN(grbrush_get_border_widths, brush, (brush, bdw));
+}
+
+
+void grbrush_get_extra_values(GrBrush *brush, ExtlTab *tab)
+{
+	*tab=extl_table_none();
+	{
+		CALL_DYN(grbrush_get_extra_values, brush, (brush, tab));
+	}
+}
+
+
+/*}}}*/
+
+
+/*{{{ Dynfuns/Borders */
+
+
+void grbrush_draw_border(GrBrush *brush, Window win, 
+						 const WRectangle *geom,
+						 const char *attrib)
+{
+	CALL_DYN(grbrush_draw_border, brush, (brush, win, geom, attrib));
+}
+
+
+/*}}}*/
+
+
+/*{{{ Dynfuns/Strings */
+
+
+void grbrush_draw_string(GrBrush *brush, Window win, int x, int y,
+						 const char *str, int len, bool needfill,
+						 const char *attrib)
+{
+	CALL_DYN(grbrush_draw_string, brush, 
+			 (brush, win, x, y, str, len, needfill, attrib));
+}
+
+
+uint grbrush_get_text_width(GrBrush *brush, const char *text, uint len)
+{
+	uint ret=0;
+	CALL_DYN_RET(ret, uint, grbrush_get_text_width, brush, 
+				 (brush, text, len));
+	return ret;
+}
+
+
+/*}}}*/
+
+
+/*{{{ Dynfuns/Textboxes */
+
+
+void grbrush_draw_textbox(GrBrush *brush, Window win, 
+						  const WRectangle *geom,
+						  const char *text, 
+						  const char *attr,
+						  bool needfill)
+{
+	CALL_DYN(grbrush_draw_textbox, brush, 
+			 (brush, win, geom, text, attr, needfill));
+}
+
+void grbrush_draw_textboxes(GrBrush *brush, Window win, 
+							const WRectangle *geom,
+							int n, const GrTextElem *elem, 
+							bool needfill, const char *common_attrib)
+{
+	CALL_DYN(grbrush_draw_textboxes, brush, 
+			 (brush, win, geom, n, elem, needfill, common_attrib));
+}
+
+
+/*}}}*/
+
+
+/*{{{ Dynfuns/Misc */
+
+
+void grbrush_set_clipping_rectangle(GrBrush *brush, Window win,
+									const WRectangle *geom)
+{
+	CALL_DYN(grbrush_set_clipping_rectangle, brush, (brush, win, geom));
+}
+
+
+void grbrush_clear_clipping_rectangle(GrBrush *brush, Window win)
+{
+	CALL_DYN(grbrush_clear_clipping_rectangle, brush, (brush, win));
+}
+
+
+void grbrush_set_window_shape(GrBrush *brush, Window win,bool rough,
+							  int n, const WRectangle *rects)
+{
+	CALL_DYN(grbrush_set_window_shape, brush, (brush, win, rough, n, rects));
+}
+
+
+void grbrush_enable_transparency(GrBrush *brush, Window win, 
+								 GrTransparency tr)
+{
+	CALL_DYN(grbrush_enable_transparency, brush, (brush, win, tr));
+}
+
+
+void grbrush_fill_area(GrBrush *brush, Window win, const WRectangle *geom,
+					   const char *attr)
+{
+	CALL_DYN(grbrush_fill_area, brush, (brush, win, geom, attr));
+}
+
+
+void grbrush_clear_area(GrBrush *brush, Window win, const WRectangle *geom)
+{
+	CALL_DYN(grbrush_clear_area, brush, (brush, win, geom));
+}
+
+
+/*}}}*/
+
+
+/*{{{ reread_draw_config */
+
+
+/*EXTL_DOC
+ * Re-read drawing engine configuration from \file{draw.conf} (or
+ * a screen-specific file).
+ */
+EXTL_EXPORT
+void reread_draw_config()
+{
+	WRootWin *rootwin;
+
+	read_config_for("draw");
+	
+	FOR_ALL_ROOTWINS(rootwin){
+		region_draw_config_updated((WRegion*)rootwin);
+	}
+}
+
+
+/*}}}*/
+
+
+/*{{{ Class implementation */
+
+
+static DynFunTab grbrush_dynfuntab[]={
+	END_DYNFUNTAB
+};
+									   
+
+IMPLOBJ(GrBrush, WObj, grbrush_deinit, grbrush_dynfuntab);
+
+
+/*}}}*/
+
---
+++ ioncore/gr.h
@@ -0,0 +1,162 @@
+/*
+ * ion/ioncore/gr.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2003. 
+ *
+ * Ion is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef ION_IONCORE_GR_H
+#define ION_IONCORE_GR_H
+
+#include "common.h"
+#include "hooks.h"
+
+
+INTROBJ(GrBrush);
+DECLOBJ(GrBrush){
+	WObj obj;
+};
+
+
+#include "rootwin.h"
+
+/* Types */
+
+#define GR_FONT_EXTENTS_INIT {0, 0, 0}
+
+typedef struct{
+	uint max_height;
+	uint max_width;
+	uint baseline;
+} GrFontExtents;
+
+#define GR_BORDER_WIDTHS_INIT {0, 0, 0, 0, 0, 0, 0}
+
+typedef struct{
+	uint top, bottom, left, right;
+	uint tb_ileft, tb_iright;
+	uint spacing;
+} GrBorderWidths;
+
+typedef struct{
+    char *text;
+    int iw;
+    const char *attr;
+} GrTextElem;
+
+typedef enum{
+	GR_TRANSPARENCY_NO,
+	GR_TRANSPARENCY_YES,
+	GR_TRANSPARENCY_DEFAULT
+} GrTransparency;
+
+
+/* Init/deinit */
+
+typedef GrBrush *GrGetBrushFn(WRootWin *rootwin, Window win, 
+							  const char *style);
+typedef bool GrGetValuesFn(WRootWin *rootwin, const char *style, 
+						   GrBorderWidths *bdw, GrFontExtents *fnte,
+						   ExtlTab *tab);
+
+extern bool gr_register_engine(const char *engine, GrGetBrushFn *fn,
+							   GrGetValuesFn *gvfn);
+extern void gr_unregister_engine(const char *engine);
+extern bool gr_select_engine(const char *engine);
+extern void reread_draw_config();
+
+extern GrBrush *gr_get_brush(WRootWin *rootwin, Window win, 
+							 const char *style);
+
+extern bool gr_get_brush_values(WRootWin *rootwin, const char *style,
+								GrBorderWidths *bdw, GrFontExtents *fnte,
+								ExtlTab *tab);
+
+extern GrBrush *grbrush_get_slave(GrBrush *brush, WRootWin *rootwin, 
+								  Window win, const char *style);
+
+extern void grbrush_release(GrBrush *brush, Window win);
+
+extern bool grbrush_init(GrBrush *brush);
+extern void grbrush_deinit(GrBrush *brush);
+
+/* Stylespecs are of the from attr1-attr2-etc. We require that each attr in
+ * 'spec' matches the one at same index in 'attrib' when '*' matches anything.
+ * The score increment for exact match is 2*3^index and 1*3^index for '*' 
+ * match. If all elements of 'spec' match those of 'attrib' exactly, the 
+ * accumulated score is returned. Otherwise the matching fails and zero is
+ * returned. For example:
+ *  
+ *  spec		attrib			score
+ * 	foo-*-baz 	foo-bar-baz		2+1*3+2*3^2 = 23
+ *  foo-bar	  	foo-bar-baz		2+2*3 		= 8
+ *  foo-baz	  	foo-bar-baz		0
+ * 
+ * gr_stylespec_score2 continues matching from attrib_p2 (if not NULL) when
+ * it has reached end of attrib.
+ */
+extern uint gr_stylespec_score(const char *spec, const char *attrib);
+extern uint gr_stylespec_score2(const char *spec, const char *attrib, 
+								const char *attrib_p2);
+
+
+/* Border drawing */
+
+DYNFUN void grbrush_get_border_widths(GrBrush *brush, GrBorderWidths *bdi);
+
+DYNFUN void grbrush_draw_border(GrBrush *brush, Window win, 
+								const WRectangle *geom,
+								const char *attrib);
+
+/* String drawing */
+
+DYNFUN void grbrush_get_font_extents(GrBrush *brush, GrFontExtents *fnti);
+
+DYNFUN void grbrush_draw_string(GrBrush *brush, Window win, int x, int y,
+								const char *str, int len, bool needfill,
+								const char *attrib);
+
+DYNFUN uint grbrush_get_text_width(GrBrush *brush, const char *text, uint len);
+
+/* Textbox drawing */
+
+DYNFUN void grbrush_draw_textbox(GrBrush *brush, Window win, 
+    	    	    	    	 const WRectangle *geom,
+								 const char *text, 
+								 const char *attr,
+    	    	    	    	 bool needfill);
+
+DYNFUN void grbrush_draw_textboxes(GrBrush *brush, Window win, 
+    	    	    	    	   const WRectangle *geom,
+    	    	    	    	   int n, const GrTextElem *elem, 
+    	    	    	    	   bool needfill, const char *common_attrib);
+
+/* Misc */
+
+DYNFUN void grbrush_set_clipping_rectangle(GrBrush *brush, Window win,
+    	    	    	    	    	   const WRectangle *geom);
+DYNFUN void grbrush_clear_clipping_rectangle(GrBrush *brush, Window win);
+
+/* Behaviour of the following two functions for "slave brushes" is undefined. 
+ * If the parameter rough to grbrush_set_window_shape is set, the actual 
+ * shape may be changed for corner smoothing and other superfluous effects.
+ * (This feature is only used by floatframes.)
+ */
+DYNFUN void grbrush_set_window_shape(GrBrush *brush, Window win, bool rough,
+    	    	    	    	     int n, const WRectangle *rects);
+
+DYNFUN void grbrush_enable_transparency(GrBrush *brush, Window win, 
+										GrTransparency mode);
+
+DYNFUN void grbrush_fill_area(GrBrush *brush, Window win, 
+							  const WRectangle *geom, const char *attr);
+DYNFUN void grbrush_clear_area(GrBrush *brush, Window win, 
+							   const WRectangle *geom);
+
+DYNFUN void grbrush_get_extra_values(GrBrush *brush, ExtlTab *tab);
+
+#endif /* ION_IONCORE_GR_H */
