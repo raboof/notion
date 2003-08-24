5b1dd0012d4ccc5665b12d451ddcd57ac560c148
tuomov <arnouten@bzzt.net>
2003-08-24 16:51:51 +0200

    trunk: changeset 909
    Menu config changes; use submenus.
    
    darcs-hash:20030824145151-e481e-3519b0a008428bfb96fe26f4fb4bcfcf90d28675.gz

--- etc/menu.lua
+++ etc/menu.lua
@@ -16,6 +16,7 @@ menu_bindings{
     kpress("Control+M", WMenu.finish),
 }
 
+include('menulib.lua')
 
 about_msg=[[
 Ion  ]]..ioncore_version()..[[ , copyright (c) Tuomo Valkonen 1999-2003.
@@ -36,21 +37,40 @@ end
 
 local appmenu={
     menuentry("XTerm", make_exec_fn("xterm")),
-    menuentry("Firebird", make_exec_fn("firebird")),
+    menuentry("Mozilla Firebird", make_exec_fn("MozillaFirebird")),
     menuentry("Xdvi", make_exec_fn("xdvi")),
     menuentry("GV", make_exec_fn("gv")),
 }
 
-
 local mainmenu={
-    menuentry("Programs>>>", make_menu_fn(appmenu)),
-    menuentry("Help...", querylib.query_man),
-    menuentry("About Ion...", about),
-    menuentry("Restart...", querylib.query_restart),
-    menuentry("Exit...", querylib.query_exit),
+    submenu("Programs", appmenu),
+    menuentry("Help", querylib.query_man),
+    menuentry("About Ion", about),
+    menuentry("Restart", querylib.query_restart),
+    menuentry("Exit", querylib.query_exit),
 }
+
+local function make_tab_fn(fn)
+    return function(frame, cwin)
+               if cwin then fn(cwin) end
+           end
+end
+
+local tabmenu={
+    menuentry("Close", make_tab_fn(WRegion.close)),
+    menuentry("Kill", make_tab_fn(WClientWin.kill)),
+    menuentry("(Un)tag", make_tab_fn(WRegion.toggle_tag)),
+    menuentry("(Un)shade", function(frame) frame:toggle_shade() end),
+}
+
 
 genframe_bindings{
+    mpress("Button3", make_pmenu_fn(tabmenu), "tab"),
+}
+
+global_bindings{
     -- Rebind F12
     kpress("F12", make_menu_fn(mainmenu)),
+    -- kpress("Menu", make_menu_fn(mainmenu)),
 }
+
