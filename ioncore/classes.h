6b2885e8012ffdeaee01eee62528f2c3917b91f8
tuomov <arnouten@bzzt.net>
2004-02-16 05:14:30 +0100

    trunk: changeset 1309
    classes.h was missing from previous commmit.
    
    darcs-hash:20040216041430-e481e-e5779196c731cfdd00681b9672a9d6031a643d1b.gz

---
+++ ioncore/classes.h
@@ -0,0 +1,32 @@
+/*
+ * ion/ioncore/classes.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2004. 
+ *
+ * Ion is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef ION_IONCORE_CLASSES_H
+#define ION_IONCORE_CLASSES_H
+
+/* Forward declarations of some classes to avoid problems
+ * with the header system.
+ */
+
+#include "obj.h"
+
+INTRCLASS(WClientWin);
+INTRCLASS(WFrame);
+INTRCLASS(WGenWS);
+INTRCLASS(WInfoWin);
+INTRCLASS(WMPlex);
+INTRCLASS(WRegion);
+INTRCLASS(WMoveresMode);
+INTRCLASS(WRootWin);
+INTRCLASS(WScreen);
+INTRCLASS(WWindow);
+
+#endif /* ION_IONCORE_CLASSES_H */
