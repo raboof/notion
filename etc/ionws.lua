788ee25ca144e5431d135811ce004ff0114bc8e7
tuomov <arnouten@bzzt.net>
2004-04-17 18:45:27 +0200

    trunk: changeset 1452
    Fixed split bindings to attach currently displayed region of old frame
    to new frame.
    
    darcs-hash:20040417164527-e481e-d5be656de5fb57de618598420110a35958946e82.gz

--- etc/ionws.lua
+++ etc/ionws.lua
@@ -6,13 +6,13 @@
 -- object on the workspace.
 
 defbindings("WIonWS", {
-    kpress(MOD1.."S",           "WIonWS.split_at(_, _sub, 'bottom')"),
+    kpress(MOD1.."S",           "WIonWS.split_at(_, _sub, 'bottom', true)"),
     kpress(MOD1.."N",           "WIonWS.goto_dir(_, 'below')"),
     kpress(MOD1.."P",           "WIonWS.goto_dir(_, 'above')"),
     kpress(MOD1.."Tab",         "WIonWS.goto_dir(_, 'right')"),
     submap(MOD1.."K", {
         kpress("AnyModifier+Tab","WIonWS.goto_dir(_, 'left')"),
-        kpress("AnyModifier+S", "WIonWS.split_at(_, _sub, 'right')"),
+        kpress("AnyModifier+S", "WIonWS.split_at(_, _sub, 'right', true)"),
         kpress("AnyModifier+X", "WIonWS.unsplit_at(_, _sub)"),
     }),
 })
