31c7deb8876261422d2c0cd2b381ef8eaf05d5c8
tuomov <arnouten@bzzt.net>
2004-05-29 21:13:02 +0200

    trunk: changeset 1522
    Check that client window is not already managed when loading saved
    configuration (on restart).
    
    darcs-hash:20040529191302-e481e-e783b051315deaec7732b436212feac6f41f0e9c.gz

--- ioncore/clientwin.c
+++ ioncore/clientwin.c
@@ -1483,6 +1483,11 @@ WRegion *clientwin_load(WWindow *par, const WFitParams *fp, ExtlTab tab)
     
     win=(Window)wind;
 
+    if(XWINDOW_REGION_OF(win)!=NULL){
+                warn("Client window %x already managed.", win);
+                return NULL;
+    }
+
     if(!xwindow_get_integer_property(win, ioncore_g.atom_checkcode, &real_chkc)
        || real_chkc!=chkc){
         if(add_cb!=NULL)
