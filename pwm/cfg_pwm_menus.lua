b7dbe7641640202e999d87344857b48eb6231b0e
tuomov <arnouten@bzzt.net>
2004-08-01 23:46:18 +0200

    trunk: changeset 1702
    Removed 'TR' from menu configuration files; include 'submenu' and
    'menuentry' in keywords to lxgettext instead.
    
    darcs-hash:20040801214618-e481e-de8d5a9b23e404804a7bce23d50833b874e884da.gz

--- pwm/cfg_pwm_menus.lua
+++ pwm/cfg_pwm_menus.lua
@@ -2,30 +2,28 @@
 -- PWM menu definitions
 --
 
--- The TR("string") calls below are used to translate the string to
--- the your chosen language, if a translation of Ion exists for it.
-
 -- Load the Ion menu setup
 dopath("cfg_menus")
 
 -- And make a new main menu with additional workspace menu.
 
 defmenu("mainmenu", {
-    submenu(TR("Programs"),     "appmenu"),
-    menuentry(TR("Lock screen"),"ioncore.exec_on(_, 'xlock')"),
-    menuentry(TR("Help"),       "mod_query.query_man(_)"),
-    menuentry(TR("About Ion"),  "mod_query.show_about_ion(_)"),
-    submenu(TR("Workspaces"),   "wsmenu"),
-    submenu(TR("Styles"),       "stylemenu"),
-    submenu(TR("Session"),      "sessionmenu"),
+    submenu("Programs",      "appmenu"),
+    menuentry("Lock screen", "ioncore.exec_on(_, 'xlock')"),
+    menuentry("Help",        "mod_query.query_man(_)"),
+    menuentry("About Ion",   "mod_query.show_about_ion(_)"),
+    submenu("Workspaces",    "wsmenu"),
+    menuentry("New",         "ioncore.create_ws(_)"),
+    submenu("Styles",        "stylemenu"),
+    submenu("Session",       "sessionmenu"),
 })
 
 
 -- Workspaces
 defmenu("wsmenu", {
-    menuentry(TR("New"),        "ioncore.create_ws(_)"),
-    menuentry(TR("Close"),      "WRegion.rqclose(_sub)",
-                                "_sub:WGenWS"),
-    submenu(TR("List"),         "workspacelist"),
+    menuentry("New",         "ioncore.create_ws(_)"),
+    menuentry("Close",       "WRegion.rqclose(_sub)",
+                             "_sub:WGenWS"),
+    submenu("List",          "workspacelist"),
 })
 
