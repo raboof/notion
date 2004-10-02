824de0624372ad29add1c90914edb9422a00f76b
tuomov <arnouten@bzzt.net>
2004-10-02 04:22:51 +0200

    trunk: changeset 1771
    Oops, history.h was missing.
    
    darcs-hash:20041002022251-e481e-a951e9b4f06c53df45db2d7c53214a42fd41def1.gz

---
+++ mod_query/history.h
@@ -0,0 +1,25 @@
+/*
+ * ion/mod_query/history.h
+ *
+ * Copyright (c) Tuomo Valkonen 1999-2004. 
+ *
+ * Ion is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef ION_MOD_QUERY_HISTORY_H
+#define ION_MOD_QUERY_HISTORY_H
+
+#include <ioncore/common.h>
+#include <ioncore/extl.h>
+
+extern const char *mod_query_history_get(int n);
+extern bool mod_query_history_push(const char *s);
+extern void mod_query_history_push_(char *s);
+extern void mod_query_history_clear();
+extern int mod_query_history_search(const char *s, int from, bool bwd);
+extern ExtlTab mod_query_history_table();
+
+#endif /* ION_MOD_QUERY_HISTORY_H */
