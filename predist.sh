5f256dfb424ac89a96f1162b674e01592202dc7a
tuomov <arnouten@bzzt.net>
2004-10-14 12:55:47 +0200

    trunk: changeset 1834
    Added -snapshot option to predist.sh.
    
    darcs-hash:20041014105547-e481e-e3ee5c658e4f39a3a733bccca0b97d0ba81e913a.gz

--- predist.sh
+++ predist.sh
@@ -4,17 +4,20 @@
 ## Versioning
 ##
 
-pwd=`pwd`
-dir=`basename "$pwd"`
+if test "$1" != "-snapshot"; then
+    pwd=`pwd`
+    dir=`basename "$pwd"`
 
-release=`echo "$dir"|sed 's/^[^-]\+-\([^-]\+-[0-9]\+\)$/\1/p; d'`
+    release=`echo "$dir"|sed 's/^[^-]\+-\([^-]\+-[0-9]\+\)$/\1/p; d'`
 
-if test "$release" == ""; then
-    echo "Invalid package name $dir."
-    exit 1
-else
-    versdef="#define ION_VERSION \"${release}\""
-    perl -p -i -e "s/^#define ION_VERSION.*/$versdef/" version.h
+    if test "$release" == ""; then
+        echo "Invalid package name $dir."
+        exit 1
+    else
+        versdef="#define ION_VERSION \"${release}\""
+        perl -p -i -e "s/^#define ION_VERSION.*/$versdef/" version.h
+        perl -p -i -e "s/ION_VERSION/$release/" configure.ac
+    fi
 fi
 
 
@@ -51,5 +54,4 @@ mv libs.mk.dist libs.mk
 ## Autoconf
 ##
 
-perl -p -i -e "s/ION_VERSION/$release/" configure.ac
 autoconf
