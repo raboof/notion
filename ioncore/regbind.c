36a7ced5346705d995869dca9128f3507fcb98de
Tuomo Valkonen <tuomov@iki.fi>
2005-03-13 22:22:27 +0100

    Alternative fix to bindmap precedence.
    - "owned" bindmaps are added last on list, while region's own bindmaps
      are listed first.
    
    darcs-hash:20050313212227-4eba8-7fc5bb51453682ed19dc6a989665a608c2a73a7c.gz

--- ioncore/regbind.c
+++ ioncore/regbind.c
@@ -143,7 +143,11 @@ bool region_add_bindmap_owned(WRegion *reg, WBindmap *bindmap, WRegion *owner)
     
     /* Link to reg's rbind list*/ {
         WRegBindingInfo *b=reg->bindings;
-        LINK_ITEM_FIRST(b, rbind, next, prev);
+        if(owner==NULL){
+            LINK_ITEM_FIRST(b, rbind, next, prev);
+        }else{
+            LINK_ITEM_LAST(b, rbind, next, prev);
+        }
         reg->bindings=b;
     }
     
