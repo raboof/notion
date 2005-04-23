86e121091b409f64b155ffdc754f33035fc6f7d0
Tuomo Valkonen <tuomov@iki.fi>
2005-04-23 14:55:16 +0200

    Added popen_bgread and exec to ion-statusd.
    darcs-hash:20050423125516-4eba8-b0a239e470479ac62bbe6e79625fc3670c0c9391.gz

---
+++ mod_statusbar/ion-statusd/exec.c
@@ -0,0 +1,39 @@
+/*
+ * ion/mod_statusbar/ion-statusd/exec.c
+ *
+ * Copyright (c) Tuomo Valkonen 2005. 
+ *
+ * Ion is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ */
+
+
+#include <libmainloop/select.h>
+#include <libmainloop/exec.h>
+
+
+/*EXTL_DOC
+ * Run \var{cmd} in the background. 
+ */
+EXTL_SAFE
+EXTL_EXPORT
+int statusd_exec(const char *cmd)
+{
+    return mainloop_spawn(cmd);
+}
+
+
+/*EXTL_DOC
+ * Run \var{cmd} with a read pipe connected to its stdout.
+ * When data is received through the pipe, \var{handler} is called
+ * with that data.
+ */
+EXTL_SAFE
+EXTL_EXPORT
+int statusd_popen_bgread(const char *cmd, ExtlFn handler)
+{
+    return mainloop_popen_bgread(cmd, handler, NULL, NULL);
+}
+
