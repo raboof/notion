dac5f3189d54231715376b9a1bb26facc71c1b90
Tuomo Valkonen <tuomov@iki.fi>
2006-09-16 17:08:27 +0200

    Use just "frame-floating" style instead of "frame-floating-groupws".
    darcs-hash:20060916150827-4eba8-3f3d3919478c2395c3b16a5ad9b360992269502c.gz

--- ioncore/floatframe.c
+++ ioncore/floatframe.c
@@ -42,7 +42,7 @@ static bool floatframe_init(WFloatFrame *frame, WWindow *parent,
     frame->tab_min_w=0;
     frame->bar_max_width_q=1.0;
     
-    if(!frame_init((WFrame*)frame, parent, fp, "frame-floating-groupws"))
+    if(!frame_init((WFrame*)frame, parent, fp, "frame-floating"))
         return FALSE;
     
     frame->frame.flags|=(FRAME_BAR_OUTSIDE|
