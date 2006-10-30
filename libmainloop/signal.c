0ebe97579c71d5ed7bd6059ce834d69a218f78f2
Tuomo Valkonen <tuomov@iki.fi>
2006-10-30 22:05:37 +0100

    Comments about moronic kernels and improved workaround
    darcs-hash:20061030210537-4eba8-0b4d0ffb92da3769b52b4cab839c62aad84823a3.gz

--- libmainloop/signal.c
+++ libmainloop/signal.c
@@ -73,12 +73,14 @@ static void do_timer_set()
         val.it_value.tv_sec=queue->when.tv_sec-val.it_value.tv_sec;
         if(val.it_value.tv_usec<0)
             val.it_value.tv_usec=0;
-        while(val.it_value.tv_usec>=USECS_IN_SEC){
-            val.it_value.tv_sec++;
-            val.it_value.tv_usec-=USECS_IN_SEC;
-        }
-        if(val.it_value.tv_sec<0)
-            val.it_value.tv_sec=0;
+        /* POSIX and some kernels have been designed by absolute morons and 
+         * contain idiotic artificial restrictions on the value of tv_usec, 
+         * that will only cause more code being run and clock cycles being 
+         * spent to do the same thing, as the kernel will in any case convert
+         * the seconds to some other units.
+         */
+         val.it_value.tv_sec+=val.it_value.tv_usec/USECS_IN_SEC;
+         val.it_value.tv_usec%=USECS_IN_SEC;
     }else{
         had_tmr=TRUE;
         return;
