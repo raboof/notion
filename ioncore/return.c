caebf79816365357156b559c7a403e87bb2e1bc1
Tuomo Valkonen <tuomov@iki.fi>
2006-12-26 18:37:06 +0100

    Use watches in return storage
    darcs-hash:20061226173706-4eba8-ed439ab0c83fb54c9be666fbe9b5628487477f64.gz

--- ioncore/return.c
+++ ioncore/return.c
@@ -15,7 +15,40 @@
 #include "pholder.h"
 #include "return.h"
 
+
+/* {{{{ Storage tree and sorting */
+
 static Rb_node retrb=NULL;
+
+
+int watch_cmp(Watch *w1, Watch *w2)
+{
+    if(w1->obj < w2->obj)
+        return -1;
+    else if(w1->obj == w2->obj)
+        return 0;
+    else
+        return 1;
+}
+
+
+static void do_remove_node(Rb_node node);
+
+
+static void retrb_watch_handler(Watch *watch, Obj *obj)
+{
+    Rb_node node;
+    int found=0;
+    
+    node=rb_find_gkey_n(retrb, &watch, (Rb_compfn*)watch_cmp, &found);
+    
+    if(found)
+        do_remove_node(node);
+}
+
+
+/*}}}*/
+
 
 
 /*{{{ Set */
@@ -24,6 +57,7 @@ static Rb_node retrb=NULL;
 bool region_do_set_return(WRegion *reg, WPHolder *ph)
 {
     Rb_node node;
+    Watch *watch;
     int found=0;
     
     region_unset_return(reg);
@@ -34,7 +68,20 @@ bool region_do_set_return(WRegion *reg, WPHolder *ph)
             return FALSE;
     }
     
-    node=rb_insertp(retrb, reg, ph);
+    watch=ALLOC(Watch);
+    
+    if(watch==NULL)
+        return FALSE;
+        
+    watch_init(watch);
+    watch_setup(watch, (Obj*)reg, &retrb_watch_handler);
+    
+    node=rb_insertg(retrb, watch, ph, (Rb_compfn*)watch_cmp);
+    
+    if(node==NULL){
+        watch_reset(watch);
+        free(watch);
+    }
     
     return (node!=NULL);
 }
@@ -70,18 +117,30 @@ extern WPHolder *region_set_return(WRegion *reg)
 
 /*{{{ Get */
 
-
-WPHolder *region_do_get_return(WRegion *reg)
+Rb_node do_find(WRegion *reg)
 {
-    Rb_node node; 
+    Watch watch;
     int found=0;
+    Rb_node node;
     
     if(retrb==NULL)
         return NULL;
     
-    node=rb_find_pkey_n(retrb, reg, &found);
+    watch_init(&watch);
     
-    return (found ? (WPHolder*)node->v.val : NULL);
+    watch.obj=(Obj*)reg;
+    
+    node=rb_find_gkey_n(retrb, &watch, (Rb_compfn*)watch_cmp, &found);
+    
+    return (found ? node : NULL);
+}
+
+
+WPHolder *region_do_get_return(WRegion *reg)
+{
+    Rb_node node=do_find(reg);
+    
+    return (node!=NULL ? (WPHolder*)node->v.val : NULL);
 }
 
 
@@ -98,22 +157,31 @@ WPHolder *region_get_return(WRegion *reg)
 /*{{{ Unset */
 
 
+static void do_remove_node(Rb_node node)
+{
+   Obj *ph=(Obj*)node->v.val;
+   Watch *watch=(Watch*)node->k.key;
+            
+   rb_delete_node(node);
+            
+   watch_reset(watch);
+   free(watch);
+            
+   if(ph!=NULL)
+       destroy_obj(ph);
+}
+
+
 void region_unset_return(WRegion *reg)
 {
     Rb_node node; 
-    int found=0;
     
-    if(retrb!=NULL){
-        node=rb_find_pkey_n(retrb, reg, &found);
+    node=do_find(reg);
     
-        if(found){
-            Obj *ph=(Obj*)node->v.val;
-            rb_delete_node(node);
-            if(ph!=NULL)
-                destroy_obj(ph);
-        }
-    }
+    if(node!=NULL)
+        do_remove_node(node);
 }
+
 
 
 /*}}}*/
