52aee6944f37b81154b2eea111c7b3d2cb82eb5f
Tuomo Valkonen <tuomov@iki.fi>
2007-02-05 19:23:45 +0100

    Removed stub appmenu.
    F3/query_exec is the Ion way. (The appmenu remains for pwm, though.)
    
    darcs-hash:20070205182345-4eba8-90d895840a094f88feb6e8989c03360e1edb9981.gz

--- etc/cfg_ioncore.lua
+++ etc/cfg_ioncore.lua
@@ -330,25 +330,13 @@ defbindings("WMoveresMode", {
 
 -- Main menu
 defmenu("mainmenu", {
-    submenu("Programs",         "appmenu"),
+    menuentry("Run...",         "mod_query.query_exec(_)"),
+    menuentry("XTerm",          "ioncore.exec_on(_, 'xterm')"),
     menuentry("Lock screen",    "ioncore.exec_on(_, 'xlock')"),
     menuentry("Help",           "mod_query.query_man(_)"),
     menuentry("About Ion",      "mod_query.show_about_ion(_)"),
     submenu("Styles",           "stylemenu"),
     submenu("Session",          "sessionmenu"),
-})
-
-
--- Application menu
-defmenu("appmenu", {
-    menuentry("XTerm",          "ioncore.exec_on(_, 'xterm')"),
-    menuentry("W3M",            "ioncore.exec_on(_, ':w3m -v')"),
-    menuentry("Rxvt",           "ioncore.exec_on(_, 'rxvt')"),
-    menuentry("Opera",          "ioncore.exec_on(_, 'opera')"),
-    menuentry("Links",          "ioncore.exec_on(_, ':links')"),
-    menuentry("Konqueror",      "ioncore.exec_on(_, 'konqueror')"),
-    menuentry("Dillo",          "ioncore.exec_on(_, 'dillo')"),
-    menuentry("Run...",         "mod_query.query_exec(_)"),
 })
 
 
--- pwm/cfg_pwm.lua
+++ pwm/cfg_pwm.lua
@@ -81,9 +81,20 @@ defmenu("mainmenu", {
     menuentry("Lock screen", "ioncore.exec_on(_, 'xlock')"),
     menuentry("Help",        "ioncore.exec_on(_, ':man pwm3')"),
     submenu("Workspaces",    "wsmenu"),
-    menuentry("New",         "ioncore.create_ws(_)"),
     submenu("Styles",        "stylemenu"),
     submenu("Session",       "sessionmenu"),
+})
+
+-- Application menu
+defmenu("appmenu", {
+    menuentry("XTerm",          "ioncore.exec_on(_, 'xterm')"),
+    menuentry("W3M",            "ioncore.exec_on(_, ':w3m -v')"),
+    menuentry("Rxvt",           "ioncore.exec_on(_, 'rxvt')"),
+    menuentry("Opera",          "ioncore.exec_on(_, 'opera')"),
+    menuentry("Links",          "ioncore.exec_on(_, ':links')"),
+    menuentry("Konqueror",      "ioncore.exec_on(_, 'konqueror')"),
+    menuentry("Dillo",          "ioncore.exec_on(_, 'dillo')"),
+    menuentry("Run...",         "mod_query.query_exec(_)"),
 })
 
 -- Workspace menu
