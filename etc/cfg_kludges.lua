2f41ed0c3cd1999f50a637dcb6211e42da58a748
Tuomo Valkonen <tuomov@iki.fi>
2007-04-22 20:07:54 +0200

    Removed possibly obsolete Galeon winprop.
    (Isn't the browser also dead already? Downwards slide started at Gnome 2.0.)
    
    darcs-hash:20070422180754-4eba8-7ff88fe1e574da51d4e90684a9528127738fbf8e.gz

--- etc/cfg_kludges.lua
+++ etc/cfg_kludges.lua
@@ -9,13 +9,6 @@ defwinprop{
     acrobatic = true
 }
 
--- Galeon's find dialog does not always have its transient_for hint 
--- set when the window is being mapped.
-defwinprop{
-    class = "galeon_browser",
-    instance = "dialog_find",
-    transient_mode = "current",
-}
 
 defwinprop{
     class = "Xpdf",
