7e2dbc825a384903c4366ac57662beef31b22048
Tuomo Valkonen <tuomov@iki.fi>
2007-11-20 19:28:08 +0100

    Clarified the full ancestry of de/fontset.c.
    (That can also be seen by examining 'changeset 956'.)
    
    darcs-hash:20071120182808-4eba8-0b94a78252900606fb6aa3bedc8315b7f007c7c3.gz

--- de/fontset.c
+++ de/fontset.c
@@ -3,10 +3,34 @@
  * 
  * This file contains routines to attempt to add fonts to a font pattern
  * so that XCreateFontSet will not fail because the given font(s) do not
- * contain all the characters required by the locale. The original code
- * was apparently written by Tomohiro Kubota; see
+ * contain all the characters required by the locale. 
+ *
+ * The original code was apparently written by Tomohiro Kubota; see
  * <http://www.debian.org/doc/manuals/intro-i18n/ch-examples.en.html#s13.4.5>.
- * 
+ *
+ * However, the code that this file is based on, was taken from:
+ *
+ * Screen.cc for Blackbox - an X11 Window manager
+ * Copyright (c) 2001 - 2002 Sean 'Shaleh' Perry <shaleh@debian.org>
+ * Copyright (c) 1997 - 2000 Brad Hughes (bhughes@tcac.net)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 
 #include <string.h>
