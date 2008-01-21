8f4a45e4149b13eb371fef17625ea5f2acc48426
Tuomo Valkonen <tuomov@iki.fi>
2008-01-21 20:38:59 +0100

    Enabled default statusbar template in cfg_statusbar.lua
    (Some seem to have trouble understanding commented-out settings.)
    
    darcs-hash:20080121193859-4eba8-02a2ca314226aef49274be023ce115fdc3ad5a6f.gz

--- etc/cfg_statusbar.lua
+++ etc/cfg_statusbar.lua
@@ -36,6 +36,7 @@ mod_statusbar.create{
     -- right, respectively, and %systray is a placeholder for system tray
     -- windows and icons.
     --
+    template="[ %date || load: %load ] %filler%systray",
     --template="[ %date || load:% %>load || mail:% %>mail_new/%>mail_total ] %filler%systray",
     --template="[ %date || load: %05load_1min || mail: %02mail_new/%02mail_total ] %filler%systray",
 }
