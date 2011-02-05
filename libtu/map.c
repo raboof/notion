b087317ef9888e4f61e3498f0ca2d179a0951b31
Tomáš Ebenlendr <ebik@ucw.cz>
2011-02-05 23:35:48 +0100

    Add StringFunPtrMap.
    Adds map between function pointers and strings. This should allow
    named alternative implementation of a function.

--- libtu/map.c
+++ libtu/map.c
@@ -45,3 +45,39 @@ const char *stringintmap_key(const StringIntMap *map, int value,
     return dflt;
     
 }
+
+
+
+int stringfunptrmap_ndx(const StringFunPtrMap *map, const char *str)
+{
+    int i;
+    
+    for(i=0; map[i].string!=NULL; i++){
+        if(strcmp(str, map[i].string)==0)
+            return i;
+    }
+    
+    return -1;
+}
+
+
+FunPtr stringfunptrmap_value(const StringFunPtrMap *map, const char *str, FunPtr dflt)
+{
+    int i=stringfunptrmap_ndx(map, str);
+    return (i==-1 ? dflt : map[i].value); 
+}
+
+
+const char *stringfunptrmap_key(const StringFunPtrMap *map, FunPtr value, 
+                             const char *dflt)
+{
+    int i;
+    
+    for(i=0; map[i].string!=NULL; ++i){
+        if(map[i].value==value){
+            return map[i].string;
+        }
+    }
+    
+    return dflt;
+}
--- libtu/map.h
+++ libtu/map.h
@@ -24,4 +24,31 @@ extern int stringintmap_value(const StringIntMap *map, const char *str,
 extern const char *stringintmap_key(const StringIntMap *map, 
                                     int value, const char *dflt);
 
+
+
+typedef void (*FunPtr)(void);
+
+#define DECLFUNPTRMAP(NAME) \
+typedef struct _String##NAME##Map{\
+    const char *string;\
+    NAME value;\
+} String##NAME##Map
+
+DECLFUNPTRMAP(FunPtr);
+
+#define END_STRINGPTRMAP {NULL, NULL}
+
+/* Return the index of str in map or -1 if not found. */
+extern int stringfunptrmap_ndx(const StringFunPtrMap *map, const char *str);
+extern FunPtr stringfunptrmap_value(const StringFunPtrMap *map, const char *str,
+                              FunPtr dflt);
+extern const char *stringfunptrmap_key(const StringFunPtrMap *map, 
+                                    FunPtr value, const char *dflt);
+
+
+#define STRINGFUNPTRMAP_NDX(MAP,STR) stringfunptrmap_ndx((StringFunPtrMap *)MAP, STR)
+#define STRINGFUNPTRMAP_VALUE(TYPE,MAP,STR,DFLT) (TYPE)stringfunptrmap_value((StringFunPtrMap *)MAP, STR, (FunPtr)DFLT)
+#define STRINGFUNPTRMAP_KEY(MAP,VALUE,DFLT) stringfunptrmap_key((StringFunPtrMap *)MAP, (FunPtr)VALUE, DFLT)
+
+
 #endif /* LIBTU_MAP_H */
