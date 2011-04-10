e7b3493eab8ba1a8e4978d3bb4e7ae1b5ab61341
Tomáš Ebenlendr <ebik@ucw.cz>
2011-04-10 16:17:09 +0200

    Funcionality split, callable by lua.
    The user may setup screens as he wants from lua. This version
    can setup screens only once, moreover it has to be done before
    mod_xrandr loads.

--- mod_xinerama/mod_xinerama.c
+++ mod_xinerama/mod_xinerama.c
@@ -4,6 +4,9 @@
  *
  * by Thomas Themel <themel0r@wannabehacker.com>
  *
+ * splitted to read and setup part callable by lua
+ * by Tomas Ebenlendr <ebik@ucw.cz>
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -29,6 +32,7 @@
 #include <ioncore/group-ws.h>
 #include <ioncore/sizepolicy.h>
 #include <ioncore/../version.h>
+#include "exports.h"
 
 #ifdef MOD_XINERAMA_DEBUG
 #include <stdio.h>
@@ -36,77 +40,109 @@
 
 char mod_xinerama_ion_api_version[]=ION_API_VERSION;
 
+/* {{{ the actual xinerama query */
 static int xinerama_event_base;
 static int xinerama_error_base;
+static bool xinerama_ready = FALSE;
 
-bool mod_xinerama_init()
+EXTL_SAFE
+EXTL_EXPORT
+ExtlTab mod_xinerama_query_screens()
 {
-    WRootWin* rootWin = ioncore_g.rootwins;
-    Display* dpy = ioncore_g.dpy;
-    int scrNum,nScreens;
     XineramaScreenInfo* sInfo;
-    WScreen* new;
+    int nRects,i;
+    if (xinerama_ready) {
+        ExtlTab ret = extl_create_table();
 
-    int nRects;
-    int i;
+        sInfo = XineramaQueryScreens(ioncore_g.dpy, &nRects);
 
-    if(XineramaQueryExtension(dpy,&xinerama_event_base, &xinerama_error_base))
-    {
-        XineramaScreenInfo* sInfo;
-        sInfo = XineramaQueryScreens(dpy, &nRects);
-
-        if(!sInfo)
-        {
-            warn(TR("Could not retrieve Xinerama screen info, sorry."));
-            return FALSE ;
-        }
-        
-        for(i = 0 ; i < nRects ; ++i)
-        {
-            WFitParams fp;
-            WMPlexAttachParams par = MPLEXATTACHPARAMS_INIT;
-
-            WScreen* newScreen;
-            WRegion* reg=NULL;
-#ifdef MOD_XINERAMA_DEBUG
-            printf("Rectangle #%d: x=%d y=%d width=%u height=%u\n", 
-                   i+1, sInfo[i].x_org, sInfo[i].y_org, sInfo[i].width,
-                   sInfo[i].height);
-#endif
-            fp.g.x = sInfo[i].x_org;
-            fp.g.y = sInfo[i].y_org;
-            fp.g.w = sInfo[i].width;
-            fp.g.h = sInfo[i].height;
-            fp.mode = REGION_FIT_EXACT;
-
-            par.flags = MPLEX_ATTACH_GEOM|MPLEX_ATTACH_SIZEPOLICY|MPLEX_ATTACH_UNNUMBERED ;
-            par.geom = fp.g;
-            par.szplcy = SIZEPOLICY_FULL_EXACT;
-
-            newScreen = (WScreen*) mplex_do_attach_new(&rootWin->scr.mplex, &par,
-                (WRegionCreateFn*)create_screen, NULL);
-
-            if(newScreen == NULL) {
-                warn(TR("Unable to create Xinerama workspace %d."), i);
-                XFree(sInfo);
-                return FALSE;
-                }
-
-            newScreen->id = i ;
-
+        if(!sInfo) return ret;
+       
+        for(i = 0 ; i < nRects ; ++i) {
+            ExtlTab rect = extl_create_table();
+            extl_table_sets_i(rect,"x_org",sInfo[i].x_org);
+            extl_table_sets_i(rect,"y_org",sInfo[i].y_org);
+            extl_table_sets_i(rect,"width",sInfo[i].width);
+            extl_table_sets_i(rect,"height",sInfo[i].height);
+            extl_table_seti_t(ret,i,rect);
         }
 
         XFree(sInfo);
-        rootWin->scr.id = -2;
+        return ret;
     }
-    else
+    return extl_table_none();
+}
+/* }}} */
+
+/* {{{ Setup ion's screens */
+/* This function has to be rewritten to be safe to
+   call more than once. We have also to move to lua
+   as much as possible. */
+static bool mod_xinerama_setup_screens(ExtlTab screens)
+{
+    WRootWin* rootWin = ioncore_g.rootwins;
+    ExtlTab screen;
+    int i;
+
+    for (i=0;extl_table_geti_t(screens,i,&screen);i++) {
+        WFitParams fp;
+        WMPlexAttachParams par = MPLEXATTACHPARAMS_INIT;
+
+        WScreen* newScreen;
+        WRegion* reg=NULL;
+        extl_table_gets_i(screen,"x_org",&(fp.g.x));
+        extl_table_gets_i(screen,"y_org",&(fp.g.y));
+        extl_table_gets_i(screen,"width",&(fp.g.w));
+        extl_table_gets_i(screen,"height",&(fp.g.h));
+        fp.mode = REGION_FIT_EXACT;
+
+#ifdef MOD_XINERAMA_DEBUG
+        printf("Rectangle #%d: x=%d y=%d width=%u height=%u\n", 
+               i+1, fp.g.x, fp.g.y, fp.g.w, fp.g.h);
+#endif
+
+        par.flags = MPLEX_ATTACH_GEOM|MPLEX_ATTACH_SIZEPOLICY|MPLEX_ATTACH_UNNUMBERED ;
+        par.geom = fp.g;
+        par.szplcy = SIZEPOLICY_FULL_EXACT;
+
+        newScreen = (WScreen*) mplex_do_attach_new(&rootWin->scr.mplex, &par,
+            (WRegionCreateFn*)create_screen, NULL);
+
+        if(newScreen == NULL) {
+            warn(TR("Unable to create Xinerama workspace %d."), i);
+            return FALSE;
+        }
+
+        newScreen->id = i ;
+
+    }
+    rootWin->scr.id = -2;
+    return TRUE;
+}
+
+static bool setup_screens_called = FALSE;
+EXTL_EXPORT
+bool mod_xinerama_setup_screens_once(ExtlTab screens)
+{
+    if (setup_screens_called) return FALSE;
+    setup_screens_called = TRUE;
+    return mod_xinerama_setup_screens(screens);
+}
+/* }}} */
+
+/* {{{ Module init and deinit */
+bool mod_xinerama_init()
+{
+    xinerama_ready = XineramaQueryExtension(ioncore_g.dpy,&xinerama_event_base, &xinerama_error_base);
+    if (!xinerama_ready)
         warn(TR("No Xinerama support detected, mod_xinerama won't do anything."));
 
-    return TRUE;
+    return mod_xinerama_register_exports();
 }
 
 
-bool mod_xinerama_deinit()
+void mod_xinerama_deinit()
 {
-    return TRUE;
+    mod_xinerama_unregister_exports();
 }
+/* }}} */
---
+++ mod_xinerama/mod_xinerama.lua
@@ -0,0 +1,49 @@
+-- Ion xinerama module - lua setup
+-- 
+-- by Tomas Ebenlendr <ebik@ucw.cz>
+--
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License as published by the Free Software Foundation; either
+-- version 2.1 of the License,or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not,write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+
+-- This is a slight abuse of the package.loaded variable perhaps, but
+-- library-like packages should handle checking if they're loaded instead of
+-- confusing the user with require/include differences.
+if package.loaded["mod_xinerama"] then return end
+
+if not ioncore.load_module("mod_xinerama") then
+    return
+end
+
+local mod_xinerama=_G["mod_xinerama"]
+
+assert(mod_xinerama)
+
+-- Helper functions {{{
+
+-- }}}
+
+
+-- Mark ourselves loaded.
+package.loaded["mod_xinerama"]=true
+
+
+-- Load configuration file
+dopath('cfg_xinerama', true)
+
+-- FIXME this function has to be called only once!
+local screens = mod_xinerama.query_screens();
+if screens then
+    mod_xinerama.setup_screens_once(screens);
+end 
