a39558b0c5d765db46160cd8f53e634037d9df3c
Tomáš Ebenlendr <ebik@ucw.cz>
2011-04-13 00:29:16 +0200

    Autodetect libtu and libextl in subdirectories.
    If libtu or libextl is in subdirectory, then automatically set up paths
    for the library.

--- build/libs.mk
+++ build/libs.mk
@@ -1,16 +1,45 @@
+LIBS_SUBDIRS =
 
-LIBS_SUBDIRS = libmainloop
+ifeq ($(shell ls $(TOPDIR)/libtu/obj.h 2>/dev/null),)
 
-LIBTU_DIR = $(TOPDIR)/../libtu-3
+#External libtu, feel free to edit
+LIBTU_DIR = $(TOPDIR)/../libtu
 LIBTU_INCLUDES = 
 LIBTU_LIBS = -ltu
+
+else
+
+#libtu as subproject
+LIBTU_DIR = $(TOPDIR)/libtu
+LIBTU_INCLUDES = -I$(TOPDIR)
+LIBTU_LIBS = -L$(LIBTU_DIR) -ltu
+LIBS_SUBDIRS += libtu
+
+endif
+
+ifeq ($(shell ls $(TOPDIR)/libextl/luaextl.h 2>/dev/null),)
+
+#External libextl, feel free to edit
+LIBEXTL_DIR = $(TOPDIR)/../libextl
+LIBEXTL_INCLUDES =
+LIBEXTL_LIBS = -lextl
+
+MKEXPORTS = libextl-mkexports
+
+else
+
+LIBEXTL_DIR = $(TOPDIR)/libextl
+LIBEXTL_INCLUDES = -I$(TOPDIR)
+LIBEXTL_LIBS = -L$(LIBEXTL_DIR) -lextl
+
+MKEXPORTS = $(LUA) $(LIBEXTL_DIR)/libextl-mkexports
+
+LIBS_SUBDIRS += libextl
+
+endif
 
 LIBMAINLOOP_DIR = $(TOPDIR)/libmainloop
 LIBMAINLOOP_INCLUDES = -I$(TOPDIR)
 LIBMAINLOOP_LIBS = -L$(LIBMAINLOOP_DIR) -lmainloop
 
-LIBEXTL_DIR = $(TOPDIR)/../libextl-3
-LIBEXTL_INCLUDES =
-LIBEXTL_LIBS = -lextl
-
-MKEXPORTS = libextl-mkexports
+LIBS_SUBDIRS += libmainloop
