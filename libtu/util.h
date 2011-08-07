9db141ce4a4033e3c1ac5b7b69d55ff85d62a27f
Arnout <arnouten@bzzt.net>
2011-08-07 14:53:02 +0200

    Document (lack of) character encoding rules in the API

--- libtu/util.h
+++ libtu/util.h
@@ -17,8 +17,23 @@
 #include "types.h"
 #include "optparser.h"
 
+/** 
+ * @parame argv0 The program name used to invoke the current program, with 
+ * path (if specified). Unfortunately it is generally not easy to determine 
+ * the encoding of this string, so we don't require a specific one here.
+ * 
+ * @see http://stackoverflow.com/questions/5408730/what-is-the-encoding-of-argv
+ */
 extern void libtu_init(const char *argv0);
+/** 
+ * The program name used to invoke the current program, with path (if 
+ * supplied). Unfortunately the encoding is undefined.
+ */
 extern const char *libtu_progname();
+/** 
+ * The program name used to invoke the current program, without path.
+ * Unfortunately the encoding is undefined. 
+ */
 extern const char *libtu_progbasename();
 
 #endif /* LIBTU_UTIL_H */
