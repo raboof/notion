9a9ec138d2446d1a2100088f53a9719bb925a023
Arnout <arnouten@bzzt.net>
2011-09-03 16:04:36 +0200

    Ion->Notion

--- ioncore/ioncore.c
+++ ioncore/ioncore.c
@@ -1,6 +1,7 @@
 /*
  * ion/ioncore/ioncore.c
  *
+ * Copyright (c) The Notion Team 2011. 
  * Copyright (c) Tuomo Valkonen 1999-2009. 
  *
  * See the included file LICENSE for details.
@@ -71,17 +72,17 @@
 
 WGlobal ioncore_g;
 
-static const char *progname="ion";
+static const char *progname="notion";
 
 static const char ioncore_copy[]=
-    "Ion " ION_VERSION ", copyright (c) Tuomo Valkonen 1999-2009.";
+    "Notion " NOTION_VERSION ", see the README for copyright details.";
 
 static const char ioncore_license[]=DUMMY_TR(
     "This software is licensed under the GNU Lesser General Public License\n"
-    "(LGPL), version 2.1, extended with terms applying to the use of the name\n"
-    "of the project, Ion(tm), unless otherwise indicated in components taken\n"
-    "from elsewhere. For details, see the file LICENSE that you should have\n"
-    "received with this software.\n"
+    "(LGPL), version 2.1, extended with terms applying to the use of the\n"
+    "former name of the project, Ion(tm), unless otherwise indicated in\n"
+    "components taken from elsewhere. For details, see the file LICENSE\n"
+    "that you should have received with this software.\n"
     "\n"
     "This program is distributed in the hope that it will be useful,\n"
     "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
@@ -670,7 +671,7 @@ void ioncore_deinit()
 
 
 /*EXTL_DOC
- * Is Ion supporting locale-specifically multibyte-encoded strings?
+ * Is Notion supporting locale-specifically multibyte-encoded strings?
  */
 EXTL_SAFE
 EXTL_EXPORT
