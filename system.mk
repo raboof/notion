5892fa08ac4fcdc3eb04597a7e6be8fbe0dde8b6
Arnout Engelen <arnouten@bzzt.net>
2012-06-09 14:34:04 +0200

    Correctly build even when lua interpreter and lua libraries are different versions
    Fixes https://sourceforge.net/tracker/?func=detail&aid=3525658&group_id=314802&atid=1324528

--- system.mk
+++ system.mk
@@ -77,28 +77,43 @@ LUAC=$(LUA_DIR)/bin/luac
 
 ifndef LUA_MANUAL
 
+# lua libraries and includes:
+
 ifeq (5.2,$(findstring 5.2,$(shell pkg-config --exists lua5.2 && pkg-config --modversion lua5.2)))
 
 LUA_LIBS=`pkg-config --libs lua5.2`
 LUA_INCLUDES=`pkg-config --cflags lua5.2`
-LUA=`which lua5.2`
-LUAC=`which luac5.2`
 
 else ifeq (5.1,$(findstring 5.1,$(shell pkg-config --exists lua5.1 && pkg-config --modversion lua5.1)))
 
 LUA_LIBS=`pkg-config --libs lua5.1`
 LUA_INCLUDES=`pkg-config --cflags lua5.1`
-LUA=`which lua5.1`
-LUAC=`which luac5.1`
 
 else ifeq (5.1,$(findstring 5.1,$(shell pkg-config --exists lua && pkg-config --modversion lua)))
 
 LUA_LIBS=`pkg-config --libs lua`
 LUA_INCLUDES=`pkg-config --cflags lua`
+
+endif # lua
+
+# lua interpreter:
+
+ifneq ($(shell which lua5.2),)
+
+LUA=`which lua5.2`
+LUAC=`which luac5.2`
+
+else ifneq ($(shell which lua5.1),)
+
+LUA=`which lua5.1`
+LUAC=`which luac5.1`
+
+else ifneq ($(shell which lua),)
+
 LUA=`which lua`
 LUAC=`which luac`
 
-endif # lua
+endif # lua interpreter
 
 endif # LUA_MANUAL
 
