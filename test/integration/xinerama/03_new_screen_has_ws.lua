be81c00376d4440b613f0a5c4e3b622495e6e823
Arnout Engelen <arnouten@bzzt.net>
2012-06-16 01:07:04 +0200

    Test for desired behavior: a new screen should get a default workspace

---
+++ test/integration/xinerama/03_new_screen_has_ws.lua
@@ -0,0 +1,14 @@
+
+mod_xinerama.refresh();
+
+if (notioncore.find_screen_id(1) == Nil) then
+  return "Number of screens should be 2, again at the start of this test"
+end
+
+if (notioncore.find_screen_id(0):mx_count() < 1) then
+  return "Screen 0 had no workspaces!"
+elseif (notioncore.find_screen_id(1):mx_count() < 1) then
+  return "Screen 1 had no workspaces!"
+end
+
+return "ok"
