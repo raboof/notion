879e9dbd82ead0ed58f51dcc40c365429a1edeed
Arnout <arnouten@bzzt.net>
2012-08-22 22:28:03 +0200

    Use GCC-specific (__cyg_profile_func_enter-based) profiling

--- ioncore/profiling.c
+++ ioncore/profiling.c
@@ -2,7 +2,13 @@
 #include "profiling.h"
 
 #ifdef PROFILING_ENABLED
-#include <google/profiler.h>
+#include <stdio.h>
+#include <time.h>
+#include <malloc.h>
+
+static FILE *fp_trace;
+struct timespec *current_time;
+
 #endif
 
 /*EXTL_DOC
@@ -12,7 +18,8 @@ EXTL_SAFE
 EXTL_EXPORT
 void ioncore_profiling_start(char* filename) {
 #ifdef PROFILING_ENABLED
-    ProfilerStart(filename);
+    current_time = malloc(sizeof(struct timespec));
+    fp_trace = fopen(filename, "w");
 #endif
 }
 
@@ -23,7 +30,34 @@ EXTL_SAFE
 EXTL_EXPORT
 void ioncore_profiling_stop() {
 #ifdef PROFILING_ENABLED
-    ProfilerStop();
+    FILE *fp_trace_to_close = fp_trace;
+    fp_trace = NULL;
+    free(current_time);
+    if(fp_trace_to_close != NULL) {
+        fflush(fp_trace_to_close);
+        fclose(fp_trace_to_close);
+    }
 #endif
 }
 
+#ifdef PROFILING_ENABLED
+void
+__cyg_profile_func_enter (void *func,  void *caller)
+{
+    if(fp_trace != NULL) {
+        clock_gettime(CLOCK_REALTIME, current_time);
+        fprintf(fp_trace, "e %p %p %ld.%lu\n", func, caller, current_time->tv_sec, current_time->tv_nsec);
+        fflush(fp_trace);
+    }
+}
+
+void
+__cyg_profile_func_exit (void *func, void *caller)
+{
+    if(fp_trace != NULL) {
+        clock_gettime(CLOCK_REALTIME, current_time);
+        fprintf(fp_trace, "x %p %p %ld.%lu\n", func, caller, current_time->tv_sec, current_time->tv_nsec);
+        fflush(fp_trace);
+    }
+}
+#endif
