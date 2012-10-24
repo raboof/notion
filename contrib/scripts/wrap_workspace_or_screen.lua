2437c250bdafc9d2212255a9aa69cc0be6c47896
Philipp Hartwig <ph@phhart.de>
2012-10-24 16:41:20 +0200

    display -> goto
    We are still looking for a better name than "display", so let's
    consistently use goto for the moment.

--- contrib/scripts/wrap_workspace_or_screen.lua
+++ contrib/scripts/wrap_workspace_or_screen.lua
@@ -50,29 +50,29 @@ function wrap_wsscr.goto_next(region, direction, param)
         if direction=='left' then
             local screen=ioncore.goto_prev_screen()
             local reg=ioncore.navi_first(underlying_manager(screen:current()), 'right')
-            if reg then reg:display() end
+            if reg then reg:goto() end
         elseif direction=='right' then
             local screen=ioncore.goto_next_screen()
             local reg=ioncore.navi_first(underlying_manager(screen:current()), 'left')
-            if reg then reg:display() end
+            if reg then reg:goto() end
         elseif direction=='up' then
             local screen=workspace:screen_of()
             local ws=underlying_manager(prev_ws(screen))
             --For floating workspaces, ioncore.navi_first only seems to work 
             --when the workspace is already displayed. Can someone explain 
             --this?
-            ws:display()
+            ws:goto()
             local reg=ioncore.navi_first(ws, 'down', {no_ascend=ws,})
-            if reg then reg:display() end
+            if reg then reg:goto() end
         elseif direction=='down' then
             local screen=workspace:screen_of()
             local ws=underlying_manager(next_ws(screen))
             --For floating workspaces, ioncore.navi_first only seems to work 
             --when the workspace is already displayed. Can someone explain 
             --this?
-            ws:display()
+            ws:goto()
             local reg=ioncore.navi_first(ws, 'up', {no_ascend=ws,})
-            if reg then reg:display() end
+            if reg then reg:goto() end
         end
     end
 end
