7d56feadc399764908284902a342e1ab2104c3ff
Arnout Engelen <arnouten@bzzt.net>
2013-05-10 20:38:20 +0200

    Fix unit tests

--- mod_xrandr/test_xrandr.lua
+++ mod_xrandr/test_xrandr.lua
@@ -9,11 +9,8 @@ local secondscreen = {
 }
 
 notioncore = { 
-  load_module = function() 
-    return 1
-  end,
-  get_hook = function()
-  end,
+  load_module = function() return 1 end,
+  get_hook = function() return { add = function() end; } end,
   region_i = function(callback, type)
       local next = callback(firstscreen)
       if next then callback(secondscreen) end
@@ -23,10 +20,12 @@ notioncore = {
 function dopath() 
 end
 
+local all_outputs = { VGA1 = { x=0, y=0, w=1680, h=1050, name='VGA1' }, LVDS1 = { x=1680, y=0, w=1920, h=1080, name='LVDS1' } } 
+
 _G["mod_xrandr"] = {
 --  query_screens = function() end
     get_all_outputs = function()
-            return { VGA1 = { x=0, y=0, w=1680, h=1050 }, LVDS1 = { x=1680, y=0, w=1920, h=1080 } }
+            return all_outputs;
         end
 }
 
@@ -40,19 +39,19 @@ local falls_within_secondscreen = falls_within(secondscreen.geom())
 assert(falls_within_secondscreen(secondscreen:geom()))
 assert(falls_within_secondscreen(firstscreen:geom())==false)
 
-local outputs_within_secondscreen = mod_xrandr.get_outputs_within(secondscreen)
+local outputs_within_secondscreen = mod_xrandr.get_outputs_within(all_outputs, secondscreen)
 assert(singleton(outputs_within_secondscreen))
 assert(firstValue(outputs_within_secondscreen).y == 0)
 assert(firstValue(outputs_within_secondscreen).x == 1680)
 
-local candidates = candidate_screens_for_output('VGA1')
+local candidates = candidate_screens_for_output(all_outputs, 'VGA1')
 assert(singleton(candidates))
 assert(firstKey(candidates) == "WScreen<1>")
 
-candidates = candidate_screens_for_outputs({'VGA1'})
+candidates = candidate_screens_for_outputs(all_outputs, {'VGA1'})
 assert(singleton(candidates))
 assert(firstKey(candidates) == "WScreen<1>")
 
-candidates = candidate_screens_for_output('LVDS1')
+candidates = candidate_screens_for_output(all_outputs, 'LVDS1')
 assert(singleton(candidates))
 assert(firstKey(candidates) == "WScreen")
