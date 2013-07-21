5dbd867f6df6b91335b8f2152cd54f946b15be94
Arnout Engelen <arnouten@bzzt.net>
2013-07-21 15:31:48 +0200

    More flexible logging subsystem
    For now we'll let this live alongside the existing libtu 'warn' infrastructure

--- de/font.c
+++ de/font.c
@@ -10,6 +10,7 @@
 
 #include <libtu/objp.h>
 #include <ioncore/common.h>
+#include <ioncore/log.h>
 #include "font.h"
 #include "fontset.h"
 #include "brush.h"
@@ -115,6 +116,7 @@ DEFont *de_load_font(const char *fontname)
     }
     
     if(ioncore_g.use_mb && !(ioncore_g.enc_utf8 && iso10646_font(fontname))){
+        LOG(DEBUG, FONT, "Loading fontset %s", fontname); 
         fontset=de_create_font_set(fontname);
         if(fontset!=NULL){
             if(XContextDependentDrawing(fontset)){
@@ -124,17 +126,18 @@ DEFont *de_load_font(const char *fontname)
             }
         }
     }else{
+        LOG(DEBUG, FONT, "Loading fontstruct %s", fontname); 
         fontstruct=XLoadQueryFont(ioncore_g.dpy, fontname);
     }
     
     if(fontstruct==NULL && fontset==NULL){
         if(strcmp(fontname, CF_FALLBACK_FONT_NAME)!=0){
             DEFont *fnt;
-            warn(TR("Could not load font \"%s\", trying \"%s\""),
+            LOG(WARN, FONT, TR("Could not load font \"%s\", trying \"%s\""),
                  fontname, CF_FALLBACK_FONT_NAME);
             fnt=de_load_font(CF_FALLBACK_FONT_NAME);
             if(fnt==NULL)
-                warn(TR("Failed to load fallback font."));
+                LOG(WARN, FONT, TR("Failed to load fallback font."));
             return fnt;
         }
         return NULL;
---
+++ ioncore/log.h
@@ -0,0 +1,34 @@
+/*
+ * notion/ioncore/log.h
+ *
+ * Copyright (c) the Notion team 2013. 
+ *
+ * See the included file LICENSE for details.
+ */
+
+#ifndef NOTION_IONCORE_LOG_H
+
+typedef enum{
+    DEBUG, /** Not usually shown, but can be useful for debugging */
+    INFO,  /** Usually shown, does not necessarily indicate an error */
+    WARN,  /** Usually shown, indicates a likely but non-fatal misconfiguration/error */
+    ERROR  /** Shown, indicates an error */
+} LogLevel;
+
+
+typedef enum{
+    GENERAL,
+    FONT
+} LogCategory;
+
+
+/** When logging from C code, don't use this function directly, use the LOG macro instead */
+extern void log_message(LogLevel level, LogCategory category, const char *file, const int line, const char* message, ...);
+
+#if __STDC_VERSION__ >= 199901L
+#define LOG(level, category, ...) log_message(level, category, __FILE__, __LINE__, __VA_ARGS__)
+#else
+extern void LOG(LogLevel level, LogCategory category, const char* message, ...);
+#endif
+
+#endif /*NOTION_IONCORE_LOG_H*/
