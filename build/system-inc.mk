180bf340d1165592db61de629e645c210f84683f
Arnout Engelen <arnouten@bzzt.net>
2013-11-02 17:13:29 +0100

    Removed old stuff from system-inc.mk
    Thanks to Olof Johansson for noticing

--- build/system-inc.mk
+++ build/system-inc.mk
@@ -1,20 +1,8 @@
-# Use system-ac.mk if it exist, system-autodetect.mk otherwise.
-
 ifndef TOPDIR
   TOPDIR=.
 endif
 
-SYSTEM_MK = $(TOPDIR)/system-autodetect.mk
-AC_SYSTEM_MK = $(TOPDIR)/build/ac/system-ac.mk
-
-ifeq ($(AC_SYSTEM_MK),$(wildcard $(AC_SYSTEM_MK)))
-  # Using system-ac.mk
-  include $(AC_SYSTEM_MK)
-else
-  # Not using system-ac.mk
-  include $(SYSTEM_MK)
-endif
-
+include $(TOPDIR)/system-autodetect.mk
 include $(TOPDIR)/build/libs.mk
 
 -include $(TOPDIR)/system-local.mk
