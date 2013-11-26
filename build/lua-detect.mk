95c1256e1b697ea2471f845ba5b3f3ed1ade11bc
Etan Reisner <deryni@unreliablesource.net>
2013-11-25 22:22:05 -0500

    LUA_INCLUDES can legitimately be empty (e.g. on gentoo).
    The rest of this file is a bit debian-specific (and friends) also but this is
    the only part that seems to get in the way at the moment. The 5.0 fallback
    makes the rest work out.

--- build/lua-detect.mk
+++ build/lua-detect.mk
@@ -28,6 +28,6 @@ ifeq ($(LUA_VERSION),5.0)
 endif
 
 LUA_LIBS     := $(or $(shell pkg-config --libs lua$(LUA_VERSION)),   $(error "pkg-config couldn't find linker flags for lua$(LUA_VERSION)!"))
-LUA_INCLUDES := $(or $(shell pkg-config --cflags lua$(LUA_VERSION)), $(error "pkg-config couldn't find compiler flags for lua$(LUA_VERSION)!"))
+LUA_INCLUDES := $(shell pkg-config --cflags lua$(LUA_VERSION))
 LUA          := $(or $(shell which lua$(LUA_VERSION)),               $(error No lua$(LUA_VERSION) interpreter found!))
 LUAC         := $(or $(shell which luac$(LUA_VERSION)),              $(error No lua$(LUA_VERSION) compiler found!))
