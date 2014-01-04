137cac3049a096af25eb55e1b0530405587dc4c0
Arnout Engelen <arnouten@bzzt.net>
2014-01-04 19:06:48 +0100

    Include mod_xrandr instead of using submodules

---
+++ mod_xrandr/mod_xrandr.lua
@@ -0,0 +1,49 @@
+-- 
+
+if package.loaded["mod_xrandr"] then return end
+
+if not notioncore.load_module("mod_xrandr") then
+    return
+end
+
+local mod_xrandr=_G["mod_xrandr"]
+
+assert(mod_xrandr)
+
+if not package.loaded["mod_xinerama"] then 
+    dopath("mod_xinerama")
+end
+
+function filter(t, predicate)
+     local result = {}
+     for k,v in pairs(t) do
+         if predicate(v) then
+	     result[k]=v
+         end
+     end
+     return result
+ end
+
+function mod_xrandr.get_outputs(screen)
+    -- get outputs based on geometry of this screen
+    return mod_xrandr.get_outputs_for_geom(screen:geom())
+end
+
+function falls_within(geom)
+    return function(output)
+      local result = output.x >= geom.x and output.y <= geom.y
+        and output.x + output.w <= geom.x + geom.w
+        and output.y + output.h <= geom.y + geom.h
+      return result;
+    end
+end
+
+function mod_xrandr.get_outputs_within(all_outputs, screen) 
+    return filter(all_outputs, falls_within(screen:geom()))
+end
+
+-- Mark ourselves loaded.
+package.loaded["mod_xrandr"]=true
+
+-- Load configuration file
+dopath("cfg_xrandr", true)
