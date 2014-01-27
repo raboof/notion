463219c7a0182ae1f10e4737b73b73a946b42fec
Dima Kogan <dima@secretsauce.net>
2014-01-27 14:15:24 -0800

    build: simplified compilation rules for toplevel rules.mk
    There were multiple %.o:%.c rules defined that more or less said the same thing.
    This patch consolidates them. There's much similar boilerplate in other modules
    (libtu/build/rules.mk for instance) that this patch doesn't touch. It would be a
    good idea to consolidate all the logic.

--- build/rules.mk
+++ build/rules.mk
@@ -136,15 +136,16 @@ executable_install:
 endif # EXECUTABLE
 
 
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
 ifdef MODULE
 
 ifneq ($(PRELOAD_MODULES),1)
 
-CC_PICFLAGS=-fPIC -DPIC
+CFLAGS += -fPIC -DPIC
 LD_SHAREDFLAGS=-shared
-
-%.o: %.c
-	$(CC) $(CC_PICFLAGS) $(CFLAGS) -c $< -o $@
 
 # notion might not link to Xext, so modules will have to link to it themselves
 # if they need it: 
@@ -162,9 +163,6 @@ else # PRELOAD_MODULES
 
 PICOPT=-fPIC -DPIC
 LINKOPT=-shared
-
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
 
 $(MODULE).a: $(OBJS) $(EXT_OBJS)
 	$(AR) $(ARFLAGS) $@ $+
@@ -187,13 +185,6 @@ else
 LUA_SOURCES += $(MODULE_STUB)
 
 endif #MODULE_STUB
-
-else # !MODULE
-
-
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
-
 
 endif# !MODULE
 
