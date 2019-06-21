4d397590274c2b002f182cb5268c1214e2dae3ce
Guillermo Bonvehí <gbonvehi@gmail.com>
2019-06-21 17:34:55 -0300

    define LUA_OK if not defined in notionflux
    Fixed compilation under Lua 5.1, it seems it's available since Lua 5.2

--- mod_notionflux/notionflux/notionflux.c
+++ mod_notionflux/notionflux/notionflux.c
@@ -44,6 +44,10 @@
 
 #include "../notionflux.h"
 
+#ifndef LUA_OK
+#define LUA_OK 0
+#endif
+
 #define die(...) fprintf(stderr, __VA_ARGS__), exit(1)
 
 #define SOCK_ATOM "_NOTION_MOD_NOTIONFLUX_SOCKET"
