34f422f8d1ffdef090692267ce9267aae20fdf8f
Arnout Engelen <arnout@bzzt.net>
2019-08-06 12:13:56 +0200

    Make sure live docs have a '.html' extension
    Not needed for firefox at least, but neater

--- ioncore/ioncore_misc.lua
+++ ioncore/ioncore_misc.lua
@@ -99,7 +99,7 @@ function ioncore.tagged_attach(reg, param)
 end
 
 function ioncore.show_live_docs(frame)
-    local filename = os.tmpname()
+    local filename = os.tmpname() .. ".html"
     local keysfile = io.open(filename, "w")
 
     keysfile:write([[
