937bd0702c52229afbec03050ef8de3ddd90c2af
Daniel Radetsky <dradetsky@gmail.com>
2019-09-09 20:17:32 -0700

    break out search dir script

--- utils/xephyr/xephyr-notion.sh
+++ utils/xephyr/xephyr-notion.sh
@@ -13,22 +13,12 @@ this_rel=$(dirname ${BASH_SOURCE[0]})
 rel_root=$this_rel/../..
 
 notion_root=$(realpath $rel_root)
-
-src_search_dirs() {
-    prefix='-searchdir '
-    ls -d \
-       $notion_root/de \
-       $notion_root/etc \
-       $notion_root/ioncore \
-       $notion_root/contrib/scripts \
-       $notion_root/mod_* \
-        | sed "s/^/$prefix/" | xargs
-}
+search_dir_script=$notion_root/utils/dev-search-dirs.sh
 
 notion_exe=$notion_root/notion/notion
 
 $notion_exe \
-$(src_search_dirs) \
+$($search_dir_script) \
 -nodefaultconfig \
 -smclientid testinxephyr \
 -session $notion_root/.session \
