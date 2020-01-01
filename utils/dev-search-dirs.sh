538b72bde869febac5697454b3fc8e6094f50c14
Arnout Engelen <arnout@bzzt.net>
2020-01-01 12:48:23 +0100

    Support custom config and ion-statusd under xephyr

--- utils/dev-search-dirs.sh
+++ utils/dev-search-dirs.sh
@@ -8,10 +8,12 @@ notion_root=$(realpath $rel_root)
 src_search_dirs() {
     prefix='-searchdir '
     ls -d \
+       $notion_root/.notion \
        $notion_root/de \
        $notion_root/etc \
        $notion_root/ioncore \
        $notion_root/contrib/scripts \
+       $notion_root/mod_statusbar/ion-statusd \
        $notion_root/mod_* \
         | sed "s/^/$prefix/" | xargs
 }
