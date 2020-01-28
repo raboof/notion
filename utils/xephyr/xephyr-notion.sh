1cd83a0fa7e352865024d74ae48b37f954450af8
Moritz Wilhelmy <mw@barfooze.de>
2020-01-28 21:31:00 +0100

    Debugging (#243)
    * Add more comprehensive debug information when compiling with DEBUG=1
    
    * utils/xephyr: Use the script's invocation name to decide debugger
    
    * add support for lldb and passing the debugger arguments

--- utils/xephyr/xephyr-notion.sh
+++ utils/xephyr/xephyr-notion.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # run the local dev build in xephyr
 #
@@ -6,6 +6,8 @@
 #
 # ./start-xephyr.sh &
 # ./xephyr-notion.sh
+
+set -e
 
 display=${NOTION_TEST_DISPLAY:-:1.0}
 
@@ -16,11 +18,27 @@ notion_root=$(realpath $rel_root)
 search_dir_script=$notion_root/utils/dev-search-dirs.sh
 
 notion_exe=$notion_root/notion/notion
+notion_args=( $($search_dir_script) \
+	-nodefaultconfig -smclientid testinxephyr \
+	-session "$notion_root/.session" -display $display )
 
-$notion_exe \
-$($search_dir_script) \
--nodefaultconfig \
--smclientid testinxephyr \
--session $notion_root/.session \
--display $display \
-$@
+debugger=none # gdb or lldb?
+[[ ${BASH_SOURCE[0]} == *-lldb.sh ]] && debugger=lldb
+[[ ${BASH_SOURCE[0]} == *-gdb.sh ]] && debugger=gdb
+
+while [[ $# -gt 0 ]]; do
+	if [[ $1 = "--" ]]; then
+      shift
+      break
+  else
+      notion_args+=("$1")
+      shift
+  fi
+done
+
+case "$debugger" in
+    lldb) lldb "$@" -- $notion_exe ${notion_args[*]} ;;
+    gdb)  gdb  "$@"    $notion_exe -ex "run ${notion_args[*]}" ;;
+    none) $notion_exe ${notion_args[*]} ;;
+    *) echo "error: unknown debugger: $debugger"; exit 1 ;;
+esac
