1689260ee057806aa676e17f846a8ab338253b57
Guillermo Bonvehí <gbonvehi@gmail.com>
2020-01-30 15:19:57 -0300

    Add utils/docker with sample dockerfiles (#249)
    - Dockerfile.make builds notion inside an ubuntu image (notion tag)
    - Dockerfile.run inherits make and installs notion (notion-run tag)
    setting the entrypoint to it.
    
    - docker-bash.sh builds/starts a notion container and drops into a
    bash prompt, you can run make test for example.
    - docker-xephyr.sh starts a notion-run container which expects to
    connect to DISPLAY=1.0 by default, you can use start-xephyr.sh
    before to test inside it.

---
+++ utils/docker/docker-xephyr.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+this_rel=$(dirname ${BASH_SOURCE[0]})
+rel_root=$this_rel/../..
+
+notion_root=$(realpath $rel_root)
+
+docker build -f Dockerfile.make -t notion $notion_root
+docker build -f Dockerfile.run -t notion-run $notion_root
+docker run --rm -it --name notion-test -v /tmp/.X11-unix:/tmp/.X11-unix notion-run
+
