e909730e2a496afd4d8bd6774b787aabfd2f7666
Moritz Wilhelmy <mw@barfooze.de>
2023-02-24 02:23:04 +0100

    Remove apparently unused no-op module mod_mgmtmode (closes #354)

--- modulelist.mk
+++ modulelist.mk
@@ -4,5 +4,4 @@
 
 MODULE_LIST = mod_tiling mod_query mod_menu \
 	      mod_dock mod_sp mod_sm de \
-	      mod_mgmtmode mod_statusbar \
-	      mod_xinerama mod_xrandr mod_xkbevents mod_notionflux
+	      mod_statusbar mod_xinerama mod_xrandr mod_xkbevents mod_notionflux
